9c08c6c03770b8018c34f49b7eb155557ee41551
danieleercoli243 <danielercoli@gmail.com>
2025-01-07 18:02:19 +0100

    tested the event listener and added a hover effect to the button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('js ok');
+// recupero il bottone dal DOM
+
+const shareBtn = document.querySelector('.share');
+
+// aggancio un ascoltatore di eventi al bottone
+shareBtn.addEventListener('click', () => {
+    
+})
