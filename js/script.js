58b7b31b0659b39d6d3da74203d991b388b7d834
danieleercoli243 <danielercoli@gmail.com>
2025-01-07 18:10:29 +0100

    made the script work

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 // recupero il bottone dal DOM
 
 const shareBtn = document.querySelector('.share');
+const cardFooter = document.querySelector('.card-footer');
+const shareBox = document.getElementById('share');
 
 // aggancio un ascoltatore di eventi al bottone
 shareBtn.addEventListener('click', () => {
-    
+    cardFooter.classList.remove('d-flex');
+    cardFooter.classList.add('d-none');
+    shareBox.classList.remove('d-none');
+    shareBox.classList.add('d-flex');
 })
