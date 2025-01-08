bd5013c2f78c580b6d303a342f15724cf402e610
danieleercoli243 <danielercoli@gmail.com>
2025-01-08 22:42:12 +0100

    fixed the links box in the smallest breakpoint

--- js/script.js
+++ js/script.js
@@ -7,8 +7,7 @@ const shareBox = document.getElementById('share');
 // aggancio un ascoltatore di eventi al bottone
 shareBtn.addEventListener('click', () => {
     // aggiungo e tolgo le classi necessarie per mostrare i link
-    cardFooter.classList.remove('d-flex');
-    cardFooter.classList.add('d-none');
+    
     shareBox.classList.remove('d-none');
     shareBox.classList.add('d-flex');
 })
